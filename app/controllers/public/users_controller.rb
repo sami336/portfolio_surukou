780f7b599539e87486a3af93acf8fc4d65e2b701
sami336 <sami.3s3.izawa@gmail.com>
2022-07-13 13:23:38 +0000

    会員マイページ作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,22 +1,31 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts 
   end
-   
+
   def edit
+    @user = current_user
   end
-  
+
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
-  
+
   def unsubscribe
   end
-  
+
   def withdraw
   end
-  
+
   private
 
   def user_params
-    params.require(:user).permit(:email, :name, :profile_image, :user_status )
+    params.require(:user).permit(:email, :name, :profile_image, :user_status, :image_book )
   end
 end
